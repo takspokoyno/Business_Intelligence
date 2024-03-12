8c45773eb9de1947ab803c4d012005b9d482935f
Vladyslav Burlaka <vladburlaka2003@gmail.com>
2024-03-13 00:07:04 +0200

    scrypt added

---
+++ scrypt.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+csv_file = 'DatasetModed.csv'
+df = pd.read_csv(csv_file)
+
+# Normalise ID column
+df['ID'] = df['ID'].str.replace('A-', '')
+
+# Normalise datetime columns
+df['Start_Time'] = pd.to_datetime(df['Start_Time'].str.replace(r'\.\d+', ''), errors='coerce')
+df['End_Time'] = pd.to_datetime(df['End_Time'].str.replace(r'\.\d+', ''), errors='coerce')
+
+# Add new column
+df['Duration'] = (df['End_Time'] - df['Start_Time']).dt.total_seconds()
+
+# Cut time values
+df['Start_Time'] = df['Start_Time'].dt.date
+
+# Drop the column
+df.drop(columns=['End_Time'], inplace=True)
+
+df.to_csv(csv_file, index=False)
